e30b1669af9346889779018ee787453e31cacc22
Gabby Grandin <ggrandin@charter.net>
2015-03-25 01:09:00 -0400

    program outline
    all comments right now (no code)

---
+++ Ant.java
@@ -0,0 +1,14 @@
+
+public class Ant {
+	
+	//variables:
+		//travelPath ???
+		//totalDistanceTaveled
+	
+	//initialize
+	
+	//move to new city
+		//update path traveled
+		//update total distance covered
+
+}
