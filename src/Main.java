10e161210adbe6df061540d9147b6548fac18bc1
Gabby Grandin <ggrandin@charter.net>
2015-03-24 23:53:35 -0400

    outline
    under construction

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+	
+	//variables:
+		/* 1: number of ants */
+		/* 2: number of iterations */
+		/* 3: (alpha) the degree of influence of the pheromone component */
+		/* 4: (beta) the degree of influence of the heuristic component */
+		/* 5: (p) the pheromone evaporation factor */
+	
+		//the elitism factor in Elitist Ant System
+	
+		/* 6: (t) the pheromone concentration on the leg connecting 2 cities (initially t = (1-p)*t
+		/* 7: (e) the factor controlling "wearing away" of pheromones in Ant Colony System (typically e = 0.1) */
+		/* 8: (q) the probability in Ant Colony System that an ant will choose the best leg for the next leg of the tour it is constructing, instead of choosing probabilistically */
+	
+		/* 9: (d) distance between 2 cities */
+		/* 10: (n) = 1/d */
+		/* 11: (N) set of unvisited cities */
+		/* 12: (b) best tour so far */
+		/* 13: (L) total length of best-so-far tour */
+		/* 14: (x) change in t on given leg in best-so-far tour */
+}
