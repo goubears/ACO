26a3477d35c46fb8efb5249f778ed048865fb7d9
Adela Yang <ayang@bowdoin.edu>
2015-04-03 01:06:47 -0400

    meep

--- ACO.java
+++ ACO.java
@@ -54,6 +54,7 @@ public class ACO {
 	//file Variables
 	private static BufferedReader reader = null;
 	private static File file;
+	private static String fileName;
 
 	//a vector that contains the info of all cities
 	private static Vector<City> place = new Vector<City>();
@@ -71,67 +72,32 @@ public class ACO {
 	private static double elitism;
 
 	//any colony variables
-	private static double tau;
 	private static double epsilon;
 	private static double qProb;
 
 	//contains the output of the algorithms
-	private static double[] output = new double[3]; 
-	//0: total iterations
-	//1: best length 
-	//2: time in seconds
+	private static double[] output = new double[2]; 
+	//0: best length 
+	//1: time in seconds
 
 	//main method
 	public static void main(String[] args) 
 	{
-		file 		= new File(args[0]);
-		algorithm 	= args[1];  					//either e or a
-		ants 		= Integer.parseInt(args[2]);	//integer
-		iterations 	= Integer.parseInt(args[3]);	//integer
-		alpha 		= Double.parseDouble(args[4]);	//double
-		beta		= Double.parseDouble(args[5]);	//double
-		rho			= Double.parseDouble(args[6]);	//double
-		optimum 	= Double.parseDouble(args[7]); 	//double
-
-		readFile(file);
-		
-		if(algorithm.equals("e"))
-		{
-			elitism = Double.parseDouble(args[8]);	//double
-			
-			//ANDREW: add optimum into your args
-			elite.elitistAnt(place, ants, elitism, alpha, beta, rho, iterations, args[0], optimum);
-			output[0] = (double)elite.getTotalIterations();
-			output[1] = elite.getBestLength();
-			output[2] = (double)elite.getDuration();
+		if(args.length != 0){
+			runCommand(args);
 		}
-		else if(algorithm.equals("a"))
-		{
-			epsilon = Double.parseDouble(args[8]);	//double
-			qProb 	= Double.parseDouble(args[9]);	//double
-			
-			antCS.acs(args[0], optimum, place, ants, iterations, alpha, beta, rho, epsilon, qProb);
-			output[0] = (double)antCS.getTotalIterations();
-			output[1] = antCS.getBestLength();
-			output[2] = (double)antCS.getTime();
+		else{
+			runTestElitism();
+			//runTestACS();
 		}
-		else
-		{ 
-			System.out.println("A valid algorithm name is needed.  e/a");
-			System.exit(0);
-		}
-
-		System.out.printf("%.0f, %f, %f",output[0], output[1], output[2]);
-		System.out.println();
-		//runTest();
 	}
 
-	public static void runTest(){
+	public static void runTestElitism(){
 		PrintStream out = System.out;
         PrintStream std = System.out;
 	    try
 	    {
-	        FileOutputStream fos = new FileOutputStream("results.csv", true); 
+	        FileOutputStream fos = new FileOutputStream("resultsElitism.csv", true); 
 	        out = new PrintStream(fos);
    		 	System.setOut(out);     
 	    }
@@ -140,7 +106,128 @@ public class ACO {
 	    	System.out.println(ex.getMessage());
 	    }
 
-	    // System.out.printf("%.0f, %f, %f",output[0], output[1], output[2]);
+	    int runs = 10;
+
+	    //fileName = "u2152.tsp";
+	    //file = new File(fileName);
+
+		//shares variables
+		algorithm = "e";
+		ants = 20;
+		iterations = 50;
+		alpha = 1;
+		beta = 3.5;
+		rho = 0.1;
+		//optimum = 64253;
+
+		//elitist ant variables
+		//elitism = 10;
+
+		 //first set of test to optimize the specialized parameters of elitist and acs
+	    for(int i=0; i<4; i++){
+	    	switch (i){
+	    		case 0: 
+	    			fileName = "u2152.tsp";
+	    			file = new File(fileName);
+	    			optimum = 64253;
+	    			break;
+	    		case 1:
+	    			fileName = "fl3795.tsp";
+	    			file = new File(fileName);
+	    			optimum = 28772;
+	    			break;
+	    		case 2:
+	    			fileName = "fnl4461.tsp";
+	    			file = new File(fileName);
+	    			optimum = 182566;
+	    			break;
+	    		case 3:
+	    			fileName = "rl5934.tsp";
+	    			file = new File(fileName);
+	    			optimum = 556045;
+	    			break;
+	    		default:
+	    			System.out.printf("Error in file name.");
+	    			break;
+	    	}
+	    	System.out.printf("\nFile Name: %s\n", fileName);
+	    	readFile(file);
+
+	    	for(int j=0; j<3; j++){
+	    		switch (j){
+	    			case 0:
+	    				elitism = 10;
+	    				break;
+	    			case 1:
+	    				elitism = 20;
+	    				break;
+	    			case 2:
+	    				elitism = 30;
+	    				break;
+	    			default: 
+	    				System.out.printf("Error in elitism factor.");
+	    				break;
+	    		}
+	    		System.out.printf("\nElitism Factor: %f\n", elitism);
+
+	    		double avgLength = 0;
+	    		double avgTime = 0;
+	    		for(int k=0; k<runs; k++){
+	    			elite.elitistAnt(place, ants, elitism, alpha, beta, rho, iterations, fileName, optimum);
+	    			avgLength += elite.getBestLength();
+					avgTime += (double)elite.getDuration();
+					System.setOut(std); 
+					float pComplete = (((float)k+1)/(float)runs)*100;
+					System.out.printf("%d%%\n", (int)pComplete);
+					System.setOut(out);
+	    		}
+	    		avgLength = avgLength/(double)runs;
+	    		avgTime = avgTime/(double)runs;
+	    		System.out.printf("\nAvgLength, AvgTime\n%f,   %f\n", avgLength, avgTime);
+
+	    	}
+	    	place.removeAllElements();
+	    }   
+	}
+
+	public static void runCommand(String[] arg){
+		file 		= new File(arg[0]);
+		algorithm 	= arg[1];  					//either e or a
+		ants 		= Integer.parseInt(arg[2]);	//integer
+		iterations 	= Integer.parseInt(arg[3]);	//integer
+		alpha 		= Double.parseDouble(arg[4]);	//double
+		beta		= Double.parseDouble(arg[5]);	//double
+		rho			= Double.parseDouble(arg[6]);	//double
+		optimum 	= Double.parseDouble(arg[7]); 	//double
+
+		readFile(file);
+		
+		if(algorithm.equals("e"))
+		{
+			elitism = Double.parseDouble(arg[8]);	//double
+			
+			//ANDREW: add optimum into your args
+			elite.elitistAnt(place, ants, elitism, alpha, beta, rho, iterations, arg[0], optimum);
+			output[0] = elite.getBestLength();
+			output[1] = (double)elite.getDuration();
+		}
+		else if(algorithm.equals("a"))
+		{
+			epsilon = Double.parseDouble(arg[8]);	//double
+			qProb 	= Double.parseDouble(arg[9]);	//double
+			
+			antCS.acs(arg[0], optimum, place, ants, iterations, alpha, beta, rho, epsilon, qProb);
+			output[0] = antCS.getBestLength();
+			output[1] = (double)antCS.getTime();
+		}
+		else
+		{ 
+			System.out.println("A valid algorithm name is needed.  e/a");
+			System.exit(0);
+		}
+
+		System.out.printf("Best Length: %.0f, Time: %f, %f",output[0], output[1]);
+		System.out.println();
 	}
 
 
